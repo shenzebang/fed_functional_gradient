49965b3979ecbc8ad76c968f68d4046534886501
shenzebang <shenzebang@gmail.com>
2021-05-25 17:12:08 -0400

    update the script for fedprox

--- script/cifar/cifar_fedprox.sh
+++ script/cifar/cifar_fedprox.sh
@@ -10,7 +10,7 @@ python run_fedprox.py --dataset cifar --homo_ratio .1 --n_workers 50 --n_global_
 # Experiment setting: N = 50, s = .1, no data augmentation
 python run_fedprox.py --dataset cifar --homo_ratio .3 --n_workers 50 --n_global_rounds 1000  --device cuda \
       --dense_hid_dims 120-84 --conv_hid_dims 6-16 --model convnet \
-      --step_size_0 2e-2 --step_per_epoch 5 --local_epoch 5 --mu 2.
+      --step_size_0 1e-2 --step_per_epoch 5 --local_epoch 5 --mu 3.
 
 
 # This script records the recommended hyperparameters for "fedprox"
