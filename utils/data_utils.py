3321a2c73f7e963c1c66351013c2fff6e88befd8
shenzebang <shenzebang@gmail.com>
2021-12-14 23:19:45 +0800

    fix a type issue

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -130,6 +130,7 @@ def make_dataloader(args, type, dataset: MyDataset):
 
 def split_dataset(args, dataset: VisionDataset, transform=None):
     data = dataset.data
+    data = data.numpy() if torch.is_tensor(data) is True else data
     label = dataset.targets
     n_workers = args.n_workers
     homo_ratio = args.homo_ratio
